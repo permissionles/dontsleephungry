13660614b86e7128cb5c702021beb58f014c2618
roshanbvadassery <roshanbvadassry@gmail.com>
2022-11-19 07:50:16 +0530

    Final touches

--- dontsleephungry/pages/index.tsx
+++ dontsleephungry/pages/index.tsx
@@ -6,15 +6,23 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Dont Sleep Hungry.org - Some ideas might work." />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Dont Sleep Hungry.org - Ending world hunger asap.</title>
+        <meta name="description" content="Some problems are infrastructure and consensus problems, you have to start somehwere. Small ideas first, then scale. Why the fuck do you need the billions first?" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
           End the fuck outta <a href="https://edition.cnn.com/2021/11/18/tech/elon-musk-world-hunger-wfp-donation/index.html">world hunger</a>
         </h1>
+
+        <h2 className={styles.title}>
+        Some problems are infrastructure and consensus problems, you have to start somehwere. Small ideas first, then scale. Why do you need the billions first?
+        </h2>
+
+        <h3 className={styles.title}>
+        Did you get something done this week?
+        </h3>
 
         <p className={styles.description}>
           Once and for all.{' '}
